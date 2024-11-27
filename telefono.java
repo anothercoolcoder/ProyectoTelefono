a9940ca28acbf4097ba7f177894fd56bc0d88927
Andres <andresalejandroninoaraujo@gmai.com>
2024-11-26 20:44:19 -0500

    tiempo

--- telefono.java
+++ telefono.java
@@ -1,5 +1,7 @@
+import java.util.Date;
 public class telefono {
     public static void main(String[] args) {
-        
+        Date currenDate = new Date();
+        System.out.println(currenDate);
     }
 }
