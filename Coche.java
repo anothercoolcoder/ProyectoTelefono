be39e703ddca7a3ad02e58d0447ab3000717fee9
Andres <andresalejandroninoaraujo@gmai.com>
2024-12-04 19:50:46 -0500

    comiteado

--- Coche.java
+++ Coche.java
@@ -15,5 +15,6 @@ public class Coche {
         System.out.println("Color "+ coche1.color);
         System.out.println("Marca "+ coche1.marca);
         System.out.println("KM "+ coche1.km);
+        sc.close();
     }
 }
