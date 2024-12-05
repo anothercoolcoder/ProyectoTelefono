eadfc1f89be8cd4fe9ea47bef73c95440930e854
Andres <andresalejandroninoaraujo@gmai.com>
2024-12-04 19:41:26 -0500

    hola

--- Coche.java
+++ Coche.java
@@ -1,18 +1,19 @@
+import java.util.Scanner;
 public class Coche {
     String color;
     String marca;
     int km;
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);        
         Coche coche1 = new Coche();
-
-        coche1.color = "Blanco";
-        coche1.marca = "Mazda";
-        coche1.km = 0;
+        
+        coche1.color = sc.nextLine();
+        coche1.marca = sc.nextLine();
+        coche1.km = sc.nextInt();
 
         System.out.println("Color "+ coche1.color);
         System.out.println("Marca "+ coche1.marca);
         System.out.println("KM "+ coche1.km);
-
     }
 }
